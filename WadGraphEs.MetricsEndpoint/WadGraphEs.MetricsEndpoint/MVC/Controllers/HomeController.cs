d7a8d7f8eea3a193aef003a181a4df88949cba86
Freek Paans <freekpaans@gmail.com>
2015-03-15 13:50:50 +0100

    configured services page

--- WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/MVC/Controllers/HomeController.cs
+++ WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/MVC/Controllers/HomeController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WadGraphEs.MetricsEndpoint.MVC.ViewModels;
 
 namespace WadGraphEs.MetricsEndpoint.MVC.Controllers {
     public class HomeController : Controller{
         public ActionResult Index() {
-            return View();
+            return View(new OverviewViewModel());
         }
     }
 }
---
+++ WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/MVC/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WadGraphEs.MetricsEndpoint.MVC.ViewModels {
+	public class OverviewViewModel {
+		public bool HasConfiguredServices{get;set;}
+	}
+}
