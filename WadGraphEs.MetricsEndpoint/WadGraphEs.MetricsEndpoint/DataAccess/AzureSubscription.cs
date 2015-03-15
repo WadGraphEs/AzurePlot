8c12913aa170eab1e2b903d94bed0427dee82bfc
Freek Paans <freekpaans@gmail.com>
2015-03-15 17:20:36 +0100

    removed arbitrary length limitations

--- WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/DataAccess/AzureSubscription.cs
+++ WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/DataAccess/AzureSubscription.cs
@@ -8,17 +8,18 @@ namespace WadGraphEs.MetricsEndpoint.DataAccess {
 	public class AzureSubscription {
 		[Key]
 		public int Id{get;set;}
+		[MaxLength]
 		public string Name { get; set; }
-
+		[MaxLength]
 		public string FromSessionId { get; set; }
-
+		[MaxLength]
 		public string AzureSubscriptionId { get; set; }
 
 		public DateTime AddedOnUtc { get; set; }
 	
 		[MaxLength]
 		public byte[] Pfx { get; set; }
-
+		[MaxLength]
 		public string Password { get; set; }
 	}
 }
