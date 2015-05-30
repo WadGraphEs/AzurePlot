c56d8d598ccf23b7f11e3dd6225b08f038e7e02a
Freek Paans <freekpaans@gmail.com>
2015-05-30 22:32:42 +0200

    introduced common

--- WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/App_Start/BundleConfig.cs
+++ WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/App_Start/BundleConfig.cs
@@ -7,37 +7,47 @@ using System.Web.Optimization;
 namespace WadGraphEs.MetricsEndpoint {
     public class BundleConfig {
         internal static void RegisterBundles(System.Web.Optimization.BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/scripts/layout").Include(
-                "~/Scripts/jquery-1.9.1.js",
-                "~/Scripts/bootstrap.js"
-            ));
+			AddScripts(bundles);
+			AddStyles(bundles);
+        }
 
-            bundles.Add(new StyleBundle("~/content/layout").Include(
-                "~/content/bootstrap.css",
-				"~/content/layout.css"
-                
-            ));
-
-            bundles.Add(new StyleBundle("~/content/setup").Include(
-                "~/content/setup.css"
-            ));
-
-			bundles.Add(new StyleBundle("~/content/login").Include(
-                "~/content/login.css"
-            ));
-
-			bundles.Add(new StyleBundle("~/content/home").Include(
-                "~/content/home.css"
-            ));
-
-			bundles.Add(new StyleBundle("~/content/dashboard/dashboard").Include(
-                "~/content/dashboard/dashboard.css"
-            ));
+		private static void AddScripts(System.Web.Optimization.BundleCollection bundles) {
+			bundles.Add(new ScriptBundle("~/scripts/layout")
+				.Include(
+					"~/Scripts/jquery-1.9.1.js",
+					"~/Scripts/bootstrap.js"
+				)
+				.IncludeDirectory("~/scripts/common","*.js")
+			);
 
 			bundles.Add(new ScriptBundle("~/scripts/dashboard/dashboard")
 				.Include("~/scripts/typeahead.bundle.js")
-				.IncludeDirectory("~/scripts/dashboard", "*.js")
+				.IncludeDirectory("~/scripts/dashboard","*.js")
 			);
-        }
+		}
+
+		private static void AddStyles(System.Web.Optimization.BundleCollection bundles) {
+			bundles.Add(new StyleBundle("~/content/layout").Include(
+				"~/content/bootstrap.css",
+				"~/content/layout.css"
+
+			));
+
+			bundles.Add(new StyleBundle("~/content/setup").Include(
+				"~/content/setup.css"
+			));
+
+			bundles.Add(new StyleBundle("~/content/login").Include(
+				"~/content/login.css"
+			));
+
+			bundles.Add(new StyleBundle("~/content/home").Include(
+				"~/content/home.css"
+			));
+
+			bundles.Add(new StyleBundle("~/content/dashboard/dashboard").Include(
+				"~/content/dashboard/dashboard.css"
+			));
+		}
     }
 }
--- WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/scripts/dashboard/chartlist.js
+++ WadGraphEs.MetricsEndpoint/WadGraphEs.MetricsEndpoint/scripts/dashboard/chartlist.js
@@ -130,45 +130,7 @@
 	}
 
 
-	var waitAll = function(deferreds, onSuccess, onFailed, onAlways){
-		var successes = [];
-		var failed = [];
-			
-		$.each(deferreds, function(idx, res) {
-			res.done(function() {
-				success(res, arguments);
-			});
-			res.fail(function() {
-				fail(res, arguments);
-			});
-		});
-
-		var success = function(res, args) {
-			successes.push({res: res, args: args});
-			checkDone();
-		}
-		var fail = function(res, args) {
-			failed.push({res: res, args: args});
-			checkDone();
-		}
-		var done = function() {
-			if(onSuccess && successes.length) {
-				onSuccess(successes);
-			}
-			if(onFailed && failed.length) {
-				onFailed(failed);
-			}
-			if(onAlways) {
-				onAlways(successes,failed);
-			}
-		}
-		var checkDone = function() {
-			if(!(successes.length + failed.length >= deferreds.length)) {
-				return;
-			}
-			done();
-		}
-	}
+	
 
 	$(function () {
 		var toAddModel = initChartList();
@@ -182,7 +144,7 @@
 		$('.add-to-dashboard-submit').on('click', function () {
 			$('body').addClass('loading');
 			
-			new waitAll(toAddModel.commit(), function(){}, function() {}, function(success, failed) {
+			new Common.WaitAll(toAddModel.commit(), function(){}, function() {}, function(success, failed) {
 				toAddModel.clear();
 				if(failed.length) {
 					alert("Failed "+ failed.length+" items");
