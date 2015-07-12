eb0f84a581d74eb612bc04fea5fb7711b30634c8
Freek Paans <freekpaans@gmail.com>
2015-07-12 14:42:40 +0200

    changed assembly info

--- AzurePlot/AzurePlot.Lib/Properties/AssemblyInfo.cs
+++ AzurePlot/AzurePlot.Lib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WadGraphEs.MetricsEndpoint.Lib")]
+[assembly: AssemblyTitle("AzurePlot.Lib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WadGraphEs.MetricsEndpoint.Lib")]
+[assembly: AssemblyProduct("AzurePlot")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
